3dae9aaf5c11c205d21b305cc00f29620969a9ea
Kan <Doublevkan@gmail.com>
2016-10-27 16:01:00 +0800

    MessageReciever was given a unique intent filter

--- messenger/src/main/java/com/kaodim/messenger/recievers/MessageReciever.java
+++ messenger/src/main/java/com/kaodim/messenger/recievers/MessageReciever.java
@@ -4,12 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.kaodim.messenger.BuildConfig;
+
 /**
  * Created by Kanskiy on 13/10/2016.
  */
 
 public class MessageReciever  extends BroadcastReceiver {
-    public static final String FILTER_MESSAGE_RECEIVER = "FILTER_MESSAGE_RECEIVER";
+    public static final String FILTER_MESSAGE_RECEIVER = "FILTER_MESSAGE_RECEIVER_"+ BuildConfig.APPLICATION_ID;
     public static final String EXTRA_CONVERSATION_ID = "extra_conversation_id";
     public static final String EXTRA_SENDER = "extra_sender";
     public static final String EXTRA_MESSAGE = "extra_message";
